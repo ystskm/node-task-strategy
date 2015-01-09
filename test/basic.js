3160a66f3fae14746fabdae7a83ee94bd327b0b4
ystskm <brilliantpenguin@gmail.com>
2015-01-09 23:42:33 +0900

    add results-db connector

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,6 @@
 var nodeunit = require('nodeunit');
-var mongodb = require('mongodb'), wrap = require('../lib/mongo-wrap');
-var Db = mongodb.Db, Server = mongodb.Server;
+//var mongodb = require('mongodb'), wrap = require('../lib/mongo-wrap');
+//var Db = mongodb.Db, Server = mongodb.Server;
 
 var Strategy = require('../index').Strategy;
 
