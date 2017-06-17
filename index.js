0159d5aa957268130db9aa70174282980de107ec
ystskm <brilliantpenguin@gmail.com>
2017-06-17 17:29:55 +0900

    many fix and improvement v0.5.0

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 /***/
 // [node-strategy] index.js
+if(typeof Promise == 'undefined') {
+  global.Promise = require('es6-promise').Promise; // require es6-promise
+}
 exports.Strategy = require('./lib/Strategy');
